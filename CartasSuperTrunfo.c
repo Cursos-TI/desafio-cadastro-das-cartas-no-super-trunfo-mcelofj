f017f8111f583e1586c2baea2a154aa55bda54c8
mcelofj <marcelofrancojunior@gmail.com>
2024-10-31 18:19:03 +0000

    Nova atualização

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -16,6 +16,7 @@ int main() {
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    printf("Super Trunfo - Países\n");
 
     return 0;
 }
